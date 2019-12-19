d32ff5b460a891d3ed4b3664306c400fbd42e17c
Ronaldo <30354892+rodatboat@users.noreply.github.com>
2019-12-18 22:16:00 -0500

    Add files via upload
    v0.01 - very messy and inefficient but it works...

---
+++ main.py
@@ -0,0 +1,29 @@
+from functions import *
+
+cont = True
+step = 0
+while(cont):
+    print("=============================================================================\n")
+    choice = int(input("MENU:"
+                   "\n(1) Follow Global Activity Users"
+                   "\n(2) Un-Follow All Randoms"
+                    "\n(3) Un-Follow Range of Randoms"
+                       "\n(4) Un-Follow All Traitors"
+                       "\n(5) Get Following Count"
+                       "\n(6) Get Follower Count"
+                       "\n(7) Exit"))
+    if choice == 1:
+        followGlobal()
+    elif choice == 2:
+        unfollowRandoms(0)
+    elif choice == 3:
+        num = int(input("How many randoms would you like to un-follow?\n"))
+        unfollowRandoms(num)
+    elif choice == 4:
+        unfollowTraitors()
+    elif choice == 5:
+        getFollowingCount()
+    elif choice == 6:
+        getFollowerCount()
+    elif choice == 7:
+        cont = False
